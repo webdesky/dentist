8fe200a74c0f85a7bba7fbd09e6b37ae295ef703
webdesky <webdeskytechnical@gmail.com>
2018-01-31 10:02:08 +0530

    header changes

--- application/modules/common/admin_header.php
+++ application/modules/common/admin_header.php
@@ -1,32 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <title>Dentist Admin Panel</title>.
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link href="<?php echo base_url('asset/jquery-ui.css')?>" rel="stylesheet">
     <link href="<?php echo base_url('asset/vendor/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+    <link href="<?php echo base_url('asset/bootstrap.min.css')?>" rel="stylesheet">
     <link href="<?php echo base_url('asset/vendor/metisMenu/metisMenu.min.css');?>" rel="stylesheet">
     <link href="<?php echo base_url('asset/dist/css/sb-admin-2.css');?>" rel="stylesheet">
-    <link href="<?php echo base_url('asset/vendor/font-awesome/css/font-awesome.min.css');?>" rel="stylesheet" type="text/css">
-     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+    <link href="<?php echo base_url('asset/vendor/font-awesome/css/font-awesome.min.css');?>" rel="stylesheet">
+    <link href="<?php echo base_url('asset/timepicker.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('asset/ckeditor/styles.css'); ?>" rel="stylesheet">
 
     <script src="<?php echo base_url('asset/vendor/jquery/jquery.min.js');?>"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="<?php echo base_url('asset/js/jquery-ui.js')?>"></script>
     <script src="<?php echo base_url('asset/vendor/bootstrap/js/bootstrap.min.js');?>"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-
-
-    <script src="<?php echo base_url('asset/ckeditor/ckeditor.js'); ?>"></script>
-    <link rel="stylesheet" href="<?php base_url('asset/ckeditor/contents.css'); ?>">
-     <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
-
+    <script src="<?php echo base_url('asset/js/datatable.js')?>"></script>
+    <script src="<?php echo base_url('asset/ckeditor/ckeditor.js'); ?>"></script>    
+    <script src="<?php echo base_url('asset/js/timepicker.js')?>"></script>
 </head>
 <body>
